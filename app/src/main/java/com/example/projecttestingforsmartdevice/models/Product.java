9758f7486d599bf3fa617ee8a1c0ce5231057248
HarounTaHa <harountaha@outlook.sa>
2020-12-26 16:06:39 +0200

    creat models

---
+++ app/src/main/java/com/example/projecttestingforsmartdevice/models/Product.java
@@ -0,0 +1,10 @@
+package com.example.projecttestingforsmartdevice.models;
+
+public class Product {
+    String productName;
+    String productCategory;
+    String productImageUrl;
+    String productDescription;
+    int productPrice;
+
+}
---
+++ app/src/main/java/com/example/projecttestingforsmartdevice/models/User.java
@@ -0,0 +1,7 @@
+package com.example.projecttestingforsmartdevice.models;
+
+public class User {
+        String userEmail;
+        String userName;
+        String userAddress;
+}

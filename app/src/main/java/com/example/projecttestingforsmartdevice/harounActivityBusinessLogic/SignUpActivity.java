6847357dab0a188efba152255a97d2dc01665429
HarounTaHa <harountaha@outlook.sa>
2020-12-27 00:25:06 +0200

    signup validator design structure method test this the incomplete structure

---
+++ app/src/main/java/com/example/projecttestingforsmartdevice/harounActivityBusinessLogic/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.projecttestingforsmartdevice.harounActivityBusinessLogic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projecttestingforsmartdevice.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.haroun_activity_sign_up);
+    }
+}
---
+++ app/src/main/java/com/example/projecttestingforsmartdevice/harounActivityBusinessLogic/signUpActivity/Validator.java
@@ -0,0 +1,17 @@
+package com.example.projecttestingforsmartdevice.harounActivityBusinessLogic.signUpActivity;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validator {
+
+
+    public boolean emailValidator(String email){
+        return false;
+
+    }
+
+    public boolean passwordValidator(String pass){
+        return false;
+    }
+}
